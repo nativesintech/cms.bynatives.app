58bd4cd33e3b6c3e38595f1a4d17716eab7c1aca
Adam Recvlohe <adam.recvlohe@pm.me>
2022-09-17 15:01:03 -0500

    Fix img src

--- components/Business.jsx
+++ components/Business.jsx
@@ -9,14 +9,14 @@ export default function Business({
   tags,
   thumbnail,
 }) {
-  // const imgSrc = new URL(thumbnail, 'http://localhost:3000')
+  const imgSrc = thumbnail.startsWith("http") ? thumbnail : `/${thumbnail}`
 
   return (
     <Card>
       <div className="flex flex-row flex-shrink overflow-hidden border-b rounded-t border-slate-600 lg:border-0 lg:rounded-none lg:border-r lg:w-1/2 h-80">
         <img
           className="self-center object-cover w-full h-full max-w-full"
-          src={thumbnail}
+          src={imgSrc}
         />
       </div>
       <div className="flex flex-col flex-1 flex-grow w-full p-6">
